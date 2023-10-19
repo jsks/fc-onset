745ea76b4e47801f475d72a1fa62e312e4c17529
Joshua Krusell <joshua.krusell@gu.se>
2023-10-19 16:33:06 +0200

    Conform conflict candidates to coding protocol
    Script to find conflict-level episodes which do not terminate in
    either a peace agreement or one-sided victory. This was originally
    written before the coding protocol was formalized. Update it in line
    with the dyadic counterpart, in particular the handling of peace
    agreements in according with doc/coding-protocol.qmd.

--- R/conflict_candidates.R
+++ R/conflict_candidates.R
@@ -1,94 +1,65 @@
 #!/usr/bin/env Rscript
 
 library(dplyr)
+library(fc)
 library(haven)
 library(readxl)
 library(tidyr)
-
-printf <- function(...) sprintf(...) |> print()
-
-consecutive <- function(x) {
-    if (anyNA(x))
-        warning("NA's in given vector")
-
-    cumsum(c(T, diff(x) != 1))
-}
 
 ###
 # Raw Data
 #
 # Peace Agreements 1975 - 2021
+#  pa_type == 1 Full peace agreement
+#  inclusive == 1 Comprehensive peace agreement
 pce <- read_xlsx("./data/raw/ucdp-peace-agreements-221.xlsx") |>
-    select(conflict_id, paid, year, ended, duration, cease, inclusive, pa_type) |>
+    select(paid, year, conflict_id, ended, pa_date, duration, cease, inclusive, pa_type, out_iss) |>
     separate_longer_delim(conflict_id, delim = ",") |>
     mutate(conflict_id = as.numeric(conflict_id),
-           year = as.numeric(year)) |>
-    group_by(conflict_id, year) |>
-    summarise(paid = paste0(paid, collapse = ","),
-              cease = max(cease),
-              inclusive = min(inclusive),
-              pa_type = min(pa_type))
+           year = as.numeric(year),
+           end_date = as.Date(duration),
+           pa_date = as.Date(pa_date),
+           duration = end_date - pa_date) |>
+    filter(inclusive == 1, pa_type == 1,
+           is.na(duration) | end_date > as.Date(paste0(year + 1, "-12-31")))
+
+pce_collapsed <- group_by(pce, conflict_id, year) |>
+    arrange(pa_date) |>
+    slice(n())
 
 # Conflict Termination Dataset 1948 - 2019
 term <- read_excel("./data/raw/ucdp-term-acd-3-2021.xlsx") |>
-    select(conflict_id, conflictep_id, year, side_a, side_b,
-           confterm, outcome, intensity_level, incompatibility, type_of_conflict) |>
-    filter(type_of_conflict %in% 3:4)
-
-# External Support 1975 - 2017
-esd <- read_dta("./data/raw/ucdp-esd-dy-181.dta") |>
-    filter(active == 1) |>
-    group_by(conflict_id, year) |>
-    summarise(ext_sup = max(ext_sup),
-              ext_f = max(ext_f),
-              ext_m = max(ext_m))
+    filter(type_of_conflict %in% 3:4) |>
+    select(conflict_id, conflictep_id, year, gwno_a = gwno_loc, side_a, side_b,
+           outcome, intensity_level, incompatibility, recur)
 
 ###
 # Join all datasets and select civil conflicts between 1975 & 2017
-full.df <- filter(pce, conflict_id %in% term$conflict_id) |>
+full.df <- filter(pce_collapsed, conflict_id %in% term$conflict_id) |>
     full_join(term, by = c("conflict_id", "year")) |>
-    left_join(esd, by = c("conflict_id", "year")) |>
-    filter(year >= 1975, year <= 2019)
+    filter(between(year, 1975, 2019))
 
-# Find comprehensive peace agreements signed by *all* conflict actors
-df <- group_by(full.df, conflict_id) |>
+info("Finished with %d conflict terminations", sum(!is.na(full.df$outcome)))
+
+filter(full.df, !is.na(gwno_a)) |>
+    write.csv("./data/conflict_episodes.csv", row.names = F)
+
+###
+# Filter out candidates for being frozen conflicts
+outcomes <- group_by(full.df, conflict_id) |>
     arrange(year) |>
     mutate(idx = consecutive(year)) |>
     group_by(idx, .add = T) |>
-    mutate(pax = !is.na(paid) & pa_type == 1 & inclusive == 1,
+    mutate(pax = !is.na(paid),
            next_pax = lead(pax, default = F)) |>
-    ungroup()
+    filter(!is.na(outcome) & !outcome %in% c(3, 4, 6), between(year, 1975, 2019), !pax, !next_pax)
 
-# Frozen Conflict Terminations
-outcomes <- filter(df, confterm == 1, side_b != "IS", !outcome %in% c(3, 4, 6),
-                   !pax, !next_pax) |>
-    group_by(conflict_id, conflictep_id) |>
+final <- group_by(outcomes, conflict_id) |>
     filter(year == min(year)) |>
-    ungroup() |>
-    select(conflict_id, year) |>
     mutate(frozen = 1)
 
-# Full dataset
-final <- left_join(df, outcomes, by = c("conflict_id", "year")) |>
-    mutate(frozen = ifelse(is.na(frozen), 0, frozen)) |>
-    group_by(conflict_id) |>
-    filter(!is.na(conflictep_id),
-           year <= if (any(frozen == 1)) min(year[frozen == 1]) else max(year))
+info("Found %d candidate outcomes, ~%.2f%% of all terminations", nrow(final),
+     100 * nrow(final) / sum(!is.na(full.df$outcome)))
 
-printf("Found %d frozen conflicts out of %d terminations",
-       sum(final$frozen), sum(!is.na(final$outcome)))
-
-filter(final, frozen == 1, year <= 2017, !is.na(ext_sup)) |>
-    write.csv("./data/conflict_level_candidates.csv", row.names = F)
-
-###
-# Collapse conflict-year panel data
-#collapsed.df <- filter(final, conflict_id != 14129) |>
-#    group_by(conflict_id) |>
-#    summarise(frozen = last(frozen),
-#              side_a = last(side_a),
-#              side_b = last(side_b),
-#              duration = n(),
-#              ext_sup = max(ext_sup, na.rm = T),
-#              ext_f = max(ext_f, na.rm = T),
-#              ext_m = max(ext_m, na.rm = T))
+select(final, conflict_id, year, side_a, side_b, outcome, frozen) |>
+    write.csv("./data/conflict_candidates.csv", row.names = F)
