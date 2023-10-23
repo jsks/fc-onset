1ba2c732d399ba4a6d1ff35b3e9609602f9607ae
Joshua Krusell <js.shirin@gmail.com>
2023-10-23 09:56:04 +0200

    BUGFIX high intensity selection for model data

--- R/probit.R
+++ R/probit.R
@@ -8,7 +8,7 @@ library(yaml)
 options(mc.cores = parallel::detectCores() - 1)
 
 input <- commandArgs(trailingOnly = T)
-schema_file <- if (length(input) == 0) "models/frozen_bin_all.yml" else input
+schema_file <- if (length(input) == 0) "models/frozen-bin-all.yml" else input
 
 stopifnot(file.exists(schema_file))
 
@@ -20,7 +20,7 @@ df <- readRDS("./data/model_data.rds") |> ungroup()
 
 ###
 # Set the Unit of analysis
-if (schema$episodes != "high_intensity")
+if (schema$episodes == "high_intensity")
     df <- filter(df, cumulative_intensity == 1)
 
 ###
@@ -37,18 +37,21 @@ state_sup <- paste0("ext_sup_s_state_", schema$treatment)
 rebel_sup <- paste0("ext_sup_s_rebel_", schema$treatment)
 
 treatments <- select(df, {{ state_sup }}, {{ rebel_sup }}) |>
-    mutate(interaction = .data[[{{ state_sup }}]] * .data[[{{ rebel_sup }}]])
+    mutate(interaction = .data[[{{ state_sup }}]] * .data[[{{ rebel_sup }}]]) |>
+    data.matrix()
 
 ###
 # Assemble model input
 data <- list(n = sum(cases),
              k = ncol(treatments),
              T = treatments[cases, ],
+             interaction_id = which(colnames(treatments) == "interaction"),
              m = ncol(X),
              X = X[cases, ],
              n_countries = n_distinct(df$gwno_a),
              country_id = as.factor(df$gwno_a[cases]) |> as.integer(),
              y = df$strict_frozen[cases])
+str(data)
 
 sprintf("posteriors/%s/model_input.RData", schema$name) |> save.image()
 
