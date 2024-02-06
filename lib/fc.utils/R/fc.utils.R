0997053fb3e4c331f99c7a4aab805a5fc8cc16d8
Joshua Krusell <js.shirin@gmail.com>
2024-02-06 21:01:56 +0100

    Roxygen has deprecated documenting NULL

--- lib/fc.utils/R/fc.utils.R
+++ lib/fc.utils/R/fc.utils.R
@@ -3,6 +3,5 @@
 #' Utility functions used both in the construction of the fc-dataset and
 #' analysis of frozen conflict onsets.
 #'
-#' @docType package
-#' @name fc.utils
-NULL
+#' @keywords internal
+"_PACKAGE"
