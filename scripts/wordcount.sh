4951a5ceb54e33ad46e3498d94622c56432970cf
Joshua Krusell <joshua.krusell@gu.se>
2024-02-29 17:25:30 +0100

    Add error checks to the wordcount script

--- scripts/wordcount.sh
+++ scripts/wordcount.sh
@@ -1,8 +1,19 @@
 #!/usr/bin/env zsh
+#
 # Approximate word count for a quarto/Rmarkdown file. Includes body
 # text count + references, abstract, and figure captions while
 # excluding appendix and code blocks.
 ###
+
+setopt err_exit
+
+if [[ $# -ne 1 ]]; then
+    print -u 2 "Missing file argument"
+    print -u 2 "Usage: $0 <file.[qmd|Rmd|md]>"
+    exit 1
+fi
+
+[[ ! -f $1 ]] && { print -u 2 "Invalid file: $1"; exit 1 }
 
 abstract=$(yq -f extract '.abstract' $1 | wc -w)
 
