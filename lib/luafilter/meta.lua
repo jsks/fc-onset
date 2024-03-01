9d5d8721e59f4eb2d1fe4df2d43ebae63616243a
Joshua Krusell <joshua.krusell@gu.se>
2024-03-01 13:45:08 +0100

    Update Latex/Pandoc partial templates

--- lib/luafilter/meta.lua
+++ lib/luafilter/meta.lua
@@ -2,12 +2,24 @@ PANDOC_VERSION:must_be_at_least '3.1.8'
 
 function shell(cmd)
    local fd = io.popen(cmd)
-   return fd:read('*a')
+   local output = fd:read("*a")
+   local rc = {fd:close()}
+
+   return rc[3], output:gsub("\n*$", "")
+end
+
+function github_base()
+   local _, url = shell("git config --get remote.origin.url")
+   return url:gsub("git@", ""):gsub(":", "/")
 end
 
 function Meta(meta)
-   local commit = shell("git rev-parse --short HEAD"):gsub("\n*$", "")
-   meta.commit = pandoc.Link(commit, "https://github.com/jsks/fc-onset/tree/" .. commit)
+   local git_status, commit = shell("git rev-parse --short HEAD")
+   if git_status == 0 then
+      local url = string.format("https://%s/tree/%s", github_base(), commit)
+      meta.commit = pandoc.Link(commit, url)
+   end
+
    meta.wordcount = shell("scripts/wordcount.sh " .. quarto.doc.input_file)
 
    return meta
