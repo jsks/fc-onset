f2f87ef4f1d2cc6e2567ac9aec5f9e27614a3450
Joshua Krusell <js.shirin@gmail.com>
2024-03-11 16:49:53 +0100

    Switch to bash for wordcount script
    I hate bash, but it's already pre-installed on debian.

--- scripts/wordcount.sh
+++ scripts/wordcount.sh
@@ -1,19 +1,22 @@
-#!/usr/bin/env zsh
+#!/usr/bin/env bash
 #
 # Approximate word count for a quarto/Rmarkdown file. Includes body
 # text count + references, abstract, and figure captions while
 # excluding appendix and code blocks.
 ###
 
-setopt err_exit
+set -eo pipefail
 
 if [[ $# -ne 1 ]]; then
-    print -u 2 "Missing file argument"
-    print -u 2 "Usage: $0 <file.[qmd|Rmd|md]>"
+    printf "Missing file argument\n" >&2
+    printf "Usage: $0 <file.[qmd|Rmd|md]>\n" >&2
     exit 1
 fi
 
-[[ ! -f $1 ]] && { print -u 2 "Invalid file: $1"; exit 1 }
+if [[ ! -f "$1" ]]; then
+    printf "Invalid file: $1\n" >&2
+    exit 1
+fi
 
 abstract=$(yq -f extract '.abstract' $1 | wc -w)
 
@@ -25,4 +28,4 @@ body=$(sed -e '/^```/,/^```/d' -e '/^#\s*Appendix/,$d' $1 | \
 # blocks
 figure_captions=$(grep '^#|' $1 | sed -e 's/^#|\s*//' -e '/^.*:/d' | wc -w)
 
-print "$((abstract + body + figure_captions))"
+printf "%d\n" "$((abstract + body + figure_captions))"
