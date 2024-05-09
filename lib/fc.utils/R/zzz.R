b9095dd057a6ffe77d28f55a7eef73dde9c4ec29
Joshua Krusell <js.shirin@gmail.com>
2024-05-09 19:11:31 +0200

    Try to override cmdstan_version in fc.utils on load

---
+++ lib/fc.utils/R/zzz.R
@@ -0,0 +1,5 @@
+.onLoad <- function(libname, pkgname) {
+    # This is only for building the project image in order to avoid
+    # including CmdStan by precompiling our model
+    assignInNamespace("cmdstan_version", function(...) "2.34.1", ns = "cmdstanr")
+}
