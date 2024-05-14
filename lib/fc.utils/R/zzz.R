d2ff52bea0103f0d152419fb6bb972b34f287e86
Joshua Krusell <js.shirin@gmail.com>
2024-05-14 18:42:33 +0200

    Compile Stan models in container

--- lib/fc.utils/R/zzz.R
+++
@@ -1,6 +0,0 @@
-.onLoad <- function(libname, pkgname) {
-    # This is only for building the project image in order to avoid
-    # including CmdStan by precompiling our model. Don't every do
-    # something like this in a real package, please.
-    utils::assignInNamespace("cmdstan_version", function(...) "2.34.1", ns = "cmdstanr")
-}
